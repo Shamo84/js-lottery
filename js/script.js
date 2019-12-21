1d30cd520fb972531ea75d1aa0b5dd39e089b6a7
Andrea Pacifico <pacifico.mensa@gmail.com>
2019-12-21 13:18:03 +0100

    done

---
+++ js/script.js
@@ -0,0 +1,268 @@
+var userNumber;
+var houseNumber;
+
+document.getElementById('num-7').addEventListener ('click',
+  function() {
+    userNumber = 7;
+    document.getElementById('scegli').classList.add("hidden");
+    document.getElementById('scegli').classList.remove("display");
+    document.getElementById('num-che-hai-scelto').classList.add("display");
+    document.getElementById('num-che-hai-scelto').classList.remove("hidden");
+    document.getElementById('tuo-numero').classList.add("display");
+    document.getElementById('tuo-numero').classList.remove("hidden");
+    document.getElementById('tuo-numero').innerHTML = userNumber;
+    document.getElementById('annulla').classList.add("display");
+    document.getElementById('annulla').classList.remove("hidden");
+    document.getElementById('gioca').classList.add("display");
+    document.getElementById('gioca').classList.remove("hidden");
+    document.getElementById('ritenta').classList.add("hidden");
+    document.getElementById('congratulazioni').classList.add("hidden");
+    document.getElementById('il-numero-estratto-e').classList.remove("display");
+    document.getElementById('numero-estratto').classList.remove("display");
+    document.getElementById('il-numero-estratto-e').classList.add("hidden");
+    document.getElementById('numero-estratto').classList.add("hidden");
+  }
+);
+document.getElementById('num-8').addEventListener ('click',
+  function() {
+    userNumber = 8;
+    document.getElementById('scegli').classList.add("hidden");
+    document.getElementById('scegli').classList.remove("display");
+    document.getElementById('num-che-hai-scelto').classList.add("display");
+    document.getElementById('num-che-hai-scelto').classList.remove("hidden");
+    document.getElementById('tuo-numero').classList.add("display");
+    document.getElementById('tuo-numero').classList.remove("hidden");
+    document.getElementById('tuo-numero').innerHTML = userNumber;
+    document.getElementById('annulla').classList.add("display");
+    document.getElementById('annulla').classList.remove("hidden");
+    document.getElementById('gioca').classList.add("display");
+    document.getElementById('gioca').classList.remove("hidden");
+    document.getElementById('ritenta').classList.add("hidden");
+    document.getElementById('congratulazioni').classList.add("hidden");
+    document.getElementById('il-numero-estratto-e').classList.remove("display");
+    document.getElementById('numero-estratto').classList.remove("display");
+    document.getElementById('il-numero-estratto-e').classList.add("hidden");
+    document.getElementById('numero-estratto').classList.add("hidden");
+  }
+);
+document.getElementById('num-9').addEventListener ('click',
+  function() {
+    userNumber = 9;
+    document.getElementById('scegli').classList.add("hidden");
+    document.getElementById('scegli').classList.remove("display");
+    document.getElementById('num-che-hai-scelto').classList.add("display");
+    document.getElementById('num-che-hai-scelto').classList.remove("hidden");
+    document.getElementById('tuo-numero').classList.add("display");
+    document.getElementById('tuo-numero').classList.remove("hidden");
+    document.getElementById('tuo-numero').innerHTML = userNumber;
+    document.getElementById('annulla').classList.add("display");
+    document.getElementById('annulla').classList.remove("hidden");
+    document.getElementById('gioca').classList.add("display");
+    document.getElementById('gioca').classList.remove("hidden");
+    document.getElementById('ritenta').classList.add("hidden");
+    document.getElementById('congratulazioni').classList.add("hidden");
+    document.getElementById('il-numero-estratto-e').classList.remove("display");
+    document.getElementById('numero-estratto').classList.remove("display");
+    document.getElementById('il-numero-estratto-e').classList.add("hidden");
+    document.getElementById('numero-estratto').classList.add("hidden");
+  }
+);
+document.getElementById('num-4').addEventListener ('click',
+  function() {
+    userNumber = 4;
+    document.getElementById('scegli').classList.add("hidden");
+    document.getElementById('scegli').classList.remove("display");
+    document.getElementById('num-che-hai-scelto').classList.add("display");
+    document.getElementById('num-che-hai-scelto').classList.remove("hidden");
+    document.getElementById('tuo-numero').classList.add("display");
+    document.getElementById('tuo-numero').classList.remove("hidden");
+    document.getElementById('tuo-numero').innerHTML = userNumber;
+    document.getElementById('annulla').classList.add("display");
+    document.getElementById('annulla').classList.remove("hidden");
+    document.getElementById('gioca').classList.add("display");
+    document.getElementById('gioca').classList.remove("hidden");
+    document.getElementById('ritenta').classList.add("hidden");
+    document.getElementById('congratulazioni').classList.add("hidden");
+    document.getElementById('il-numero-estratto-e').classList.remove("display");
+    document.getElementById('numero-estratto').classList.remove("display");
+    document.getElementById('il-numero-estratto-e').classList.add("hidden");
+    document.getElementById('numero-estratto').classList.add("hidden");
+  }
+);
+document.getElementById('num-5').addEventListener ('click',
+  function() {
+    userNumber = 5;
+    document.getElementById('scegli').classList.add("hidden");
+    document.getElementById('scegli').classList.remove("display");
+    document.getElementById('num-che-hai-scelto').classList.add("display");
+    document.getElementById('num-che-hai-scelto').classList.remove("hidden");
+    document.getElementById('tuo-numero').classList.add("display");
+    document.getElementById('tuo-numero').classList.remove("hidden");
+    document.getElementById('tuo-numero').innerHTML = userNumber;
+    document.getElementById('annulla').classList.add("display");
+    document.getElementById('annulla').classList.remove("hidden");
+    document.getElementById('gioca').classList.add("display");
+    document.getElementById('gioca').classList.remove("hidden");
+    document.getElementById('ritenta').classList.add("hidden");
+    document.getElementById('congratulazioni').classList.add("hidden");
+    document.getElementById('il-numero-estratto-e').classList.remove("display");
+    document.getElementById('numero-estratto').classList.remove("display");
+    document.getElementById('il-numero-estratto-e').classList.add("hidden");
+    document.getElementById('numero-estratto').classList.add("hidden");
+  }
+);
+document.getElementById('num-6').addEventListener ('click',
+  function() {
+    userNumber = 6;
+    document.getElementById('scegli').classList.add("hidden");
+    document.getElementById('scegli').classList.remove("display");
+    document.getElementById('num-che-hai-scelto').classList.add("display");
+    document.getElementById('num-che-hai-scelto').classList.remove("hidden");
+    document.getElementById('tuo-numero').classList.add("display");
+    document.getElementById('tuo-numero').classList.remove("hidden");
+    document.getElementById('tuo-numero').innerHTML = userNumber;
+    document.getElementById('annulla').classList.add("display");
+    document.getElementById('annulla').classList.remove("hidden");
+    document.getElementById('gioca').classList.add("display");
+    document.getElementById('gioca').classList.remove("hidden");
+    document.getElementById('ritenta').classList.add("hidden");
+    document.getElementById('congratulazioni').classList.add("hidden");
+    document.getElementById('il-numero-estratto-e').classList.remove("display");
+    document.getElementById('numero-estratto').classList.remove("display");
+    document.getElementById('il-numero-estratto-e').classList.add("hidden");
+    document.getElementById('numero-estratto').classList.add("hidden");
+  }
+);
+document.getElementById('num-1').addEventListener ('click',
+  function() {
+    userNumber = 1;
+    document.getElementById('scegli').classList.add("hidden");
+    document.getElementById('scegli').classList.remove("display");
+    document.getElementById('num-che-hai-scelto').classList.add("display");
+    document.getElementById('num-che-hai-scelto').classList.remove("hidden");
+    document.getElementById('tuo-numero').classList.add("display");
+    document.getElementById('tuo-numero').classList.remove("hidden");
+    document.getElementById('tuo-numero').innerHTML = userNumber;
+    document.getElementById('annulla').classList.add("display");
+    document.getElementById('annulla').classList.remove("hidden");
+    document.getElementById('gioca').classList.add("display");
+    document.getElementById('gioca').classList.remove("hidden");
+    document.getElementById('ritenta').classList.add("hidden");
+    document.getElementById('congratulazioni').classList.add("hidden");
+    document.getElementById('il-numero-estratto-e').classList.remove("display");
+    document.getElementById('numero-estratto').classList.remove("display");
+    document.getElementById('il-numero-estratto-e').classList.add("hidden");
+    document.getElementById('numero-estratto').classList.add("hidden");
+  }
+);
+document.getElementById('num-2').addEventListener ('click',
+  function() {
+    userNumber = 2;
+    document.getElementById('scegli').classList.add("hidden");
+    document.getElementById('scegli').classList.remove("display");
+    document.getElementById('num-che-hai-scelto').classList.add("display");
+    document.getElementById('num-che-hai-scelto').classList.remove("hidden");
+    document.getElementById('tuo-numero').classList.add("display");
+    document.getElementById('tuo-numero').classList.remove("hidden");
+    document.getElementById('tuo-numero').innerHTML = userNumber;
+    document.getElementById('annulla').classList.add("display");
+    document.getElementById('annulla').classList.remove("hidden");
+    document.getElementById('gioca').classList.add("display");
+    document.getElementById('gioca').classList.remove("hidden");
+    document.getElementById('ritenta').classList.add("hidden");
+    document.getElementById('congratulazioni').classList.add("hidden");
+    document.getElementById('il-numero-estratto-e').classList.remove("display");
+    document.getElementById('numero-estratto').classList.remove("display");
+    document.getElementById('il-numero-estratto-e').classList.add("hidden");
+    document.getElementById('numero-estratto').classList.add("hidden");
+  }
+);
+document.getElementById('num-3').addEventListener ('click',
+  function() {
+    userNumber = 3;
+    document.getElementById('scegli').classList.add("hidden");
+    document.getElementById('scegli').classList.remove("display");
+    document.getElementById('num-che-hai-scelto').classList.add("display");
+    document.getElementById('num-che-hai-scelto').classList.remove("hidden");
+    document.getElementById('tuo-numero').classList.add("display");
+    document.getElementById('tuo-numero').classList.remove("hidden");
+    document.getElementById('tuo-numero').innerHTML = userNumber;
+    document.getElementById('annulla').classList.add("display");
+    document.getElementById('annulla').classList.remove("hidden");
+    document.getElementById('gioca').classList.add("display");
+    document.getElementById('gioca').classList.remove("hidden");
+    document.getElementById('ritenta').classList.add("hidden");
+    document.getElementById('congratulazioni').classList.add("hidden");
+    document.getElementById('il-numero-estratto-e').classList.remove("display");
+    document.getElementById('numero-estratto').classList.remove("display");
+    document.getElementById('il-numero-estratto-e').classList.add("hidden");
+    document.getElementById('numero-estratto').classList.add("hidden");
+  }
+);
+document.getElementById('num-0').addEventListener ('click',
+  function() {
+    userNumber = 0;
+    document.getElementById('scegli').classList.add("hidden");
+    document.getElementById('scegli').classList.remove("display");
+    document.getElementById('num-che-hai-scelto').classList.add("display");
+    document.getElementById('num-che-hai-scelto').classList.remove("hidden");
+    document.getElementById('tuo-numero').classList.add("display");
+    document.getElementById('tuo-numero').classList.remove("hidden");
+    document.getElementById('tuo-numero').innerHTML = userNumber;
+    document.getElementById('annulla').classList.add("display");
+    document.getElementById('annulla').classList.remove("hidden");
+    document.getElementById('gioca').classList.add("display");
+    document.getElementById('gioca').classList.remove("hidden");
+    document.getElementById('ritenta').classList.add("hidden");
+    document.getElementById('congratulazioni').classList.add("hidden");
+    document.getElementById('il-numero-estratto-e').classList.remove("display");
+    document.getElementById('numero-estratto').classList.remove("display");
+    document.getElementById('il-numero-estratto-e').classList.add("hidden");
+    document.getElementById('numero-estratto').classList.add("hidden");
+  }
+);
+document.getElementById('annulla').addEventListener ('click',
+  function() {
+    userNumber = '';
+    document.getElementById('scegli').classList.add("display");
+    document.getElementById('scegli').classList.remove("hidden");
+    document.getElementById('num-che-hai-scelto').classList.add("hidden");
+    document.getElementById('num-che-hai-scelto').classList.remove("display");
+    document.getElementById('tuo-numero').classList.add("hidden");
+    document.getElementById('tuo-numero').classList.remove("display");
+    document.getElementById('annulla').classList.add("hidden");
+    document.getElementById('annulla').classList.remove("display");
+    document.getElementById('gioca').classList.add("hidden");
+    document.getElementById('gioca').classList.remove("display");
+    document.getElementById('ritenta').classList.add("hidden");
+    document.getElementById('ritenta').classList.remove("display");
+    document.getElementById('congratulazioni').classList.add("hidden");
+    document.getElementById('congratulazioni').classList.remove("display");
+    document.getElementById('il-numero-estratto-e').classList.add("hidden");
+    document.getElementById('il-numero-estratto-e').classList.remove("display");
+    document.getElementById('numero-estratto').classList.add("hidden");
+    document.getElementById('numero-estratto').classList.remove("display");
+  }
+);
+document.getElementById('gioca').addEventListener ('click',
+  function() {
+    houseNumber = Math.floor(Math.random() * 10);
+    document.getElementById('il-numero-estratto-e').classList.add("display");
+    document.getElementById('il-numero-estratto-e').classList.remove("hidden");
+    document.getElementById('numero-estratto').innerHTML=houseNumber;
+    document.getElementById('numero-estratto').classList.add("display");
+    document.getElementById('numero-estratto').classList.remove("hidden");
+    if (houseNumber == userNumber) {
+      document.getElementById('congratulazioni').classList.add("display");
+      document.getElementById('congratulazioni').classList.remove("hidden");
+      document.getElementById('ritenta').classList.add("hidden");
+      document.getElementById('ritenta').classList.remove("display");
+
+    } else {
+      document.getElementById('congratulazioni').classList.add("hidden");
+      document.getElementById('congratulazioni').classList.remove("display");
+      document.getElementById('ritenta').classList.add("display");
+      document.getElementById('ritenta').classList.remove("hidden");
+    }
+  }
+);
